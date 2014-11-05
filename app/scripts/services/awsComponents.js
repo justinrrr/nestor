d5983b93795b952124ab9ba052b54407231d56da
Fathalian <fatalian@uw.edu>
2014-11-04 20:25:01 -0800

    Some generative stuff for the template

--- app/scripts/services/awsComponents.js
+++ app/scripts/services/awsComponents.js
@@ -8,117 +8,135 @@
 var app = angular.module('nestorApp.services');
 app.service('AWSComponents', function () {
 
-  this.tasks = [
-    {
-      name: 'Setup a machine and connect it to a DNS Address'
-    },
-    {
-      name: 'Setup a VPC'
-    },
-    {
-      name: 'Setup a machine and connect it to dynamoDB databases'
-    },
-    {
-      name: 'Create a server cluster with ELBs'
-    }
-  ];
-
-  this.components = [
-    {
-      name: 'Autoscaling Group',
-      image: 'images/aws/autoscaling.png',
-      description: 'Scale your EC2 capacity up or down automatically according to conditions you define'
-    },
-    //{
-    //  name: 'Cloud Formation',
-    //  image: 'images/aws/cloudformation.png',
-    //  description: 'Import existing infrastructure templates'
-    //},
-    //{
-    //  name: 'Cloud Front',
-    //  image: 'images/aws/cloudfront.png',
-    //  description: 'Content Delivery Network for your AWS resources'
-    //},
-    //{
-    //  name: 'Cloud Search',
-    //  image: 'images/aws/cloudsearch.png',
-    //  description: 'Service to set up, manage, and scale a custom search solution'
-    //},
-    {
-      name: 'DynamoDb',
-      image: 'images/aws/dynamo.png',
-      description: 'NoSQL database service',
-      properties: {
-        typeName:  'AWS::DynamoDB::Table'
-
+    this.tasks = [
+      {
+        name: 'Setup a machine and connect it to a DNS Address'
+      },
+      {
+        name: 'Setup a VPC'
+      },
+      {
+        name: 'Setup a machine and connect it to dynamoDB databases'
+      },
+      {
+        name: 'Create a server cluster with ELBs'
       }
-    },
-    //{
-    //  name: 'EBS',
-    //  image: 'images/aws/ebs.png',
-    //  description: 'Persistent block level storage volumes for EC2 instances'
-    //
-    //},
-    {
-      name: 'EC2',
-      image: 'images/aws/ec2.png',
-      description: 'Resizable compute machines'
-    },
-    {
-      name: 'Elastic IP',
-      image: 'images/aws/eip.png',
-      description: 'Static IP address that are dynamically allocated'
-    },
-    //{
-    //  name: 'Elasticache',
-    //  image: 'images/aws/elasticache.png',
-    //  description: 'Deploy, operate, and scale an in-memory cache'
-    //},
-    {
-      name: 'ELB',
-      image: 'images/aws/elb.png',
-      description: 'Automatically distributes incoming application traffic across multiple EC2 instances'
-    },
-    //{
-    //  name: 'rds',
-    //  image: 'images/aws/rds.png',
-    //  description: 'Set up, operate, and scale a relational database'
-    //},
-    //{
-    //  name: 'redshift',
-    //  image: 'images/aws/redshift.png',
-    //  description: 'fully managed, petabyte-scale data warehouse solution'
-    //},
-    {
-      name: 'route53',
-      image: 'images/aws/route53.png',
-      description: 'highly available and scalable Domain Name System (DNS)'
-    },
-    //{
-    //  name: 'S3',
-    //  image: 'images/aws/s3.png',
-    //  description: 'Secure, durable, highly-scalable object storage'
-    //},
-    //{
-    //  name: 'SES',
-    //  image: 'images/aws/ses.png',
-    //  description: 'Outbound-only email-sending service'
-    //},
-    //{
-    //  name: 'SNS',
-    //  image: 'images/aws/sns.png',
-    //  description: 'Fully managed push messaging service'
-    //},
-    //{
-    //  name: 'SQS',
-    //  image: 'images/aws/sqs.png',
-    //  description: 'Fast, reliable, scalable, fully managed message queuing service'
-    //},
-    {
-      name: 'VPC',
-      image: 'images/aws/vpc.png',
-      description: 'Logically isolated section of the Cloud to launch resources in a virtual network that you define'
-    },
-  ];
+    ];
 
-});
+    this.components = [
+      {
+        name: 'Autoscaling Group',
+        image: 'images/aws/autoscaling.png',
+        description: 'Scale your EC2 capacity up or down automatically according to conditions you define'
+      },
+      //{
+      //  name: 'Cloud Formation',
+      //  image: 'images/aws/cloudformation.png',
+      //  description: 'Import existing infrastructure templates'
+      //},
+      //{
+      //  name: 'Cloud Front',
+      //  image: 'images/aws/cloudfront.png',
+      //  description: 'Content Delivery Network for your AWS resources'
+      //},
+      //{
+      //  name: 'Cloud Search',
+      //  image: 'images/aws/cloudsearch.png',
+      //  description: 'Service to set up, manage, and scale a custom search solution'
+      //},
+      {
+        name: 'DynamoDb',
+        image: 'images/aws/dynamo.png',
+        description: 'NoSQL database service'
+      },
+      //{
+      //  name: 'EBS',
+      //  image: 'images/aws/ebs.png',
+      //  description: 'Persistent block level storage volumes for EC2 instances'
+      //
+      //},
+      {
+        name: 'EC2',
+        image: 'images/aws/ec2.png',
+        description: 'Resizable compute machines'
+      },
+      {
+        name: 'Elastic IP',
+        image: 'images/aws/eip.png',
+        description: 'Static IP address that are dynamically allocated'
+      },
+      //{
+      //  name: 'Elasticache',
+      //  image: 'images/aws/elasticache.png',
+      //  description: 'Deploy, operate, and scale an in-memory cache'
+      //},
+      {
+        name: 'ELB',
+        image: 'images/aws/elb.png',
+        description: 'Automatically distributes incoming application traffic across multiple EC2 instances'
+      },
+      //{
+      //  name: 'rds',
+      //  image: 'images/aws/rds.png',
+      //  description: 'Set up, operate, and scale a relational database'
+      //},
+      //{
+      //  name: 'redshift',
+      //  image: 'images/aws/redshift.png',
+      //  description: 'fully managed, petabyte-scale data warehouse solution'
+      //},
+      {
+        name: 'route53',
+        image: 'images/aws/route53.png',
+        description: 'highly available and scalable Domain Name System (DNS)'
+      },
+      //{
+      //  name: 'S3',
+      //  image: 'images/aws/s3.png',
+      //  description: 'Secure, durable, highly-scalable object storage'
+      //},
+      //{
+      //  name: 'SES',
+      //  image: 'images/aws/ses.png',
+      //  description: 'Outbound-only email-sending service'
+      //},
+      //{
+      //  name: 'SNS',
+      //  image: 'images/aws/sns.png',
+      //  description: 'Fully managed push messaging service'
+      //},
+      //{
+      //  name: 'SQS',
+      //  image: 'images/aws/sqs.png',
+      //  description: 'Fast, reliable, scalable, fully managed message queuing service'
+      //},
+      {
+        name: 'VPC',
+        image: 'images/aws/vpc.png',
+        description: 'Logically isolated section of the Cloud to launch resources in a virtual network that you define'
+      }
+    ];
+
+    //add component specific metadat here
+    this.componentMetadata = {
+      'DynamoDb': {
+        type: 'AWS::DynamoDB::Table',
+        properties: {
+          required: {
+            AttributeDefinitions: 'default',
+            KeySchema: 'default',
+            ProvisionedThroughput: 'default'
+
+          },
+          optional: {
+            GlobalSecondaryIndexes: 'default',
+            LocalSecondaryIndexes: 'default',
+            TableName: 'default'
+          }
+        }
+      }
+    };
+
+  }
+)
+;
