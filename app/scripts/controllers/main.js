845c6a284542bb66c1cb463c7a51ac285e5aa1d7
Fathalian <fatalian@uw.edu>
2014-11-21 19:03:57 -0800

    Fixed a bug related to dragging derived properties

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -319,7 +319,7 @@ angular.module('nestorApp')
 
       $scope.propertyDidDrag = function (data, event) {
 
-        var leftPanelWidth = angular.element('#right-column')[0].clientWidth;
+        var leftPanelWidth = angular.element('#left-panel')[0].clientWidth;
 
         var uniqueId = generateComponentName(data.name);
 
