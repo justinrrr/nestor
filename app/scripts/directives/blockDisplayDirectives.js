c8893ad4567291d8dfd7a9cacf66cf316843b409
Fathalian <fatalian@uw.edu>
2014-11-22 22:52:36 -0800

    added the ability to add component type as a class to divs

--- app/scripts/directives/blockDisplayDirectives.js
+++ app/scripts/directives/blockDisplayDirectives.js
@@ -11,14 +11,23 @@ app.directive('blockDisplay', function () {
   return {
     restrict: 'A',
     scope : {
-      componentBlockType: '@'
+      componentBlockType: '@',
+      componentType: '@'
     },
     link: function postLink(scope, elem, attrs) {
-      elem.resizable();
-      alert(scope.componentBlockType);
-      elem.on('resizestop', function (evt, ui) {
-        if (scope.callback) { scope.callback(); }
-      });
+
+      //the type is like aws::ec2::vpc. What we need to do is to strip the :: and replace with
+      //-. And also add the component type to the class because of the stupid ng-class limitiations
+      var componentType = scope.componentType.replace(/::/g,'-');
+      elem.addClass(componentType);
+      if (scope.componentBlockType === 'container') {
+
+        elem.resizable();
+        elem.addClass('block-style-container')
+        //elem.on('resizestop', function (evt, ui) {
+        //  if (scope.callback) { scope.callback(); }
+        //});
+      }
     }
   };
 });
